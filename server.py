d839610985f1a50c7de1b3364c2437b9411494fa
PEOPLE3D <admin.fotonature@gmail.com>
2019-08-17 20:45:49 +0600

    Added login check and last messages

--- server.py
+++ server.py
@@ -63,25 +63,31 @@ class Client(LineOnlyReceiver):
         # если логин еще не зарегистрирован
         if self.login is None:
             if message.startswith("login:"):  # проверяем, чтобы в начале шел login:
-                self.login = message.replace("login:", "")  # вырезаем часть после :
-
-                notification = f"New user: {self.login}"  # формируем уведомление о новом клиенте
-                self.factory.notify_all_users(notification)  # отсылаем всем в чат
+                tmp_login = message.replace("login:", "")  # вырезаем часть после :
+                if any(tmp_login in str(reg.login) for reg in self.factory.clients):
+                    self.sendLine(f"Логин {tmp_login} занят, попробуйте другой".encode())
+                    reactor.callLater(0.5, self.transport.loseConnection)
+                else:
+                    self.login = tmp_login
+                    notification = f"New user: {self.login}"  # формируем уведомление о новом клиенте
+                    self.factory.notify_all_users(notification)  # отсылаем всем в чат
+                    self.factory.send_history(self)  # последние сообщения
             else:
                 self.sendLine("Invalid login".encode())  # шлем уведомление, если в сообщении ошибка
-        # если логин уже есть и это следующее сообщение
-        else:
+        else:  # если логин уже есть и это следующее сообщение
             format_message = f"{self.login}: {message}"  # форматируем сообщение от имени клиента
 
             # отсылаем всем в чат и в консоль сервера
             self.factory.notify_all_users(format_message)
             print(format_message)
+            self.factory.messages.append(format_message)
 
 
 class Server(ServerFactory):
     """Класс для управления сервером"""
 
     clients: list  # список клиентов
+    messages: list  # сообщения
     protocol = Client  # протокол обработки клиента
 
     def __init__(self):
@@ -93,7 +99,7 @@ class Server(ServerFactory):
         """
 
         self.clients = []  # создаем пустой список клиентов
-
+        self.messages = []
         print("Server started - OK")  # уведомление в консоль сервера
 
     def startFactory(self):
@@ -112,6 +118,17 @@ class Server(ServerFactory):
         # отправим всем подключенным клиентам
         for user in self.clients:
             user.sendLine(data)
+
+    def send_history(self, client: Client):
+        cnt = 10
+        if len(self.messages) - cnt > 0:
+            begin = len(self.messages) - cnt
+        else:
+            begin = 0
+        if len(self.messages) > 0:
+            client.sendLine(f"Last {len(self.messages) - begin} messages:".encode())
+            for mes in self.messages[begin:len(self.messages)]:
+                client.sendLine(mes.encode())
 
 
 if __name__ == '__main__':
